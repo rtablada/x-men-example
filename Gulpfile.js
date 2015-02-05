7e1c89aa7fa294a73b369e0c1c10556ad0f6858c
Ryan Tablada <ryan.tablada@gmail.com>
2015-02-05 02:08:15 -0600

    Notifications could be cool

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,28 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
-    mocha = require('gulp-mocha');
+    notify = require('gulp-notify'),
+    mocha = require('gulp-mocha'),
+    _ = require('lodash');
 
-gulp.task('tests', function() {
+gulp.task('mocha', function() {
     return gulp.src(['tests/**/*.js'], { read: false })
-        .pipe(mocha({ reporter: 'nyan' }));
+        .pipe(mocha({ reporter: 'nyan' }))
+        .on('error', notify.onError(testNotification('fail', 'mocha')))
+        .pipe(notify(testNotification('pass', 'mocha')));
 });
 
 gulp.task('watch', function() {
-    watch(['tests/**/*.js', 'app/**/*.js'], ['tests']);
+    watch(['tests/**/*.js', 'app/**/*.js'], ['mocha']);
 });
 
-gulp.task('default', ['tests', 'watch']);
+gulp.task('default', ['mocha', 'watch']);
+
+
+function testNotification(status, pluginName) {
+    var options = {
+        title:   ( status == 'pass' ) ? 'Tests Passed' : 'Tests Failed',
+        message: ( status == 'pass' ) ? '\n\nAll tests have passed!\n\n' : '\n\nOne or more tests failed...\n\n',
+        icon:    __dirname + '/node_modules/gulp-' + pluginName +'/assets/test-' + status + '.png'
+    };
+    return options;
+}
