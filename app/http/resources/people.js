d065456a69a8ec52f01f12f5cb5c19ebb4108b5e
Ryan Tablada <ryan.tablada@gmail.com>
2015-11-07 22:59:06 -0600

    Clean up unused vars

--- app/http/resources/people.js
+++ app/http/resources/people.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var Mongoose = require('mongoose');
-var _ = require('lodash');
-var url = require('url');
-var Mystique = require('mystique');
 
 router.get('/', function(req, res) {
   return req.store.recordCollection('Person', {
